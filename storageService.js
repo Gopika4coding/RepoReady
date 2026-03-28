7f5b626df5e07d4184f2eb4b1244557d99b8b53f
Gopika D S <92445756+Gopika4coding@users.noreply.github.com>
2026-03-28 22:32:52 +0530

    Update storageService.js

--- storageService.js
+++ storageService.js
@@ -1,7 +1,1 @@
-function saveResult(data) {
-    localStorage.setItem("lastAssessment", JSON.stringify(data));
-}
 
-function loadResult() {
-    return JSON.parse(localStorage.getItem("lastAssessment"));
-}
